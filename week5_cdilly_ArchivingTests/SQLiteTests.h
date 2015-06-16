49222795ca35c3149c588d8584e9c75c41dfcc88
Robert Holzhauser <Robert@Wisdomstrategy.com>
2015-06-16 09:57:34 -0600

    unit tests

---
+++ week5_cdilly_ArchivingTests/SQLiteTests.h
@@ -0,0 +1,13 @@
+//
+//  SQLiteTests.h
+//  week5_cdilly_Archiving
+//
+//  Created by A Student on 6/16/15.
+//  Copyright (c) 2015 msse650. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface SQLiteTests : NSObject
+
+@end
---
+++ week5_cdilly_ArchivingTests/SQLiteTests.m
@@ -0,0 +1,26 @@
+//
+//  SQLiteTests.m
+//  week5_cdilly_Archiving
+//
+//  Created by A Student on 6/16/15.
+//  Copyright (c) 2015 msse650. All rights reserved.
+//
+
+#import "SQLiteTests.h"
+#import "Note.h"
+#import "NoteSvcSQLite.h"
+
+@implementation SQLiteTests
+
+-(void)testNoteSvcSQLLite{
+    NSLog(@" ");
+    NSLog(@"*** Starting testNoteSvcSQLite ***");
+    
+    // todo
+    NoteSvcSQLite *noteSvc =[[NoteSvcSQLite alloc] init];
+    
+    NSLog(@"*** Ending testNoteSvcSQLite ***");
+    NSLog(@" ");
+}
+
+@end
