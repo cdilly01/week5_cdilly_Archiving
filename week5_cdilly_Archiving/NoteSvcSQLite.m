ecbc9da6b2347a892e29bea20a8e750d00fea016
Robert Holzhauser <Robert@Wisdomstrategy.com>
2015-06-15 23:50:34 -0600

    week 6 sql lite

---
+++ week5_cdilly_Archiving/NoteSvcSQLite.m
@@ -0,0 +1,27 @@
+//
+//  NoteSvcSQLite.m
+//  week5_cdilly_Archiving
+//
+//  Created by A Student on 6/15/15.
+//  Copyright (c) 2015 msse650. All rights reserved.
+//
+
+#import "NoteSvcSQLite.h"
+#import "sqlite3.h"
+
+@implementation NoteSvcSQLite
+
+
+-(Note *) addNote: (Note *) note{
+    return note;
+}
+
+-(Note *) deleteNote: (Note *) note{
+    return note;
+}
+
+-(NSMutableArray *) retrieveAllNotes{
+    return nil;
+}
+
+@end
