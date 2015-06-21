61ecf18ac4bce8af8cd79bb3945b9fb63e9693b0
Robert Holzhauser <Robert@Wisdomstrategy.com>
2015-06-21 14:18:13 -0600

    add h and m svc files

---
+++ week5_cdilly_Archiving/NoteSvcCoreData.h
@@ -0,0 +1,12 @@
+//
+//  NoteSvcCoreData.h
+//  week7_cdilly_CoreData
+//
+//  Created by A Student on 6/21/15.
+//  Copyright (c) 2015 msse650. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+@interface NoteSvcCoreData: NSObject
+
+@end
---
+++ week5_cdilly_Archiving/NoteSvcCoreData.m
@@ -0,0 +1,16 @@
+//
+//  NoteSvcCoreData.m
+//  week7_cdilly_CoreData
+//
+//  Created by A Student on 6/21/15.
+//  Copyright (c) 2015 msse650. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "NoteSvcCoreData.h"
+
+@implementation NoteSvcCoreData
+
+
+
+@end
