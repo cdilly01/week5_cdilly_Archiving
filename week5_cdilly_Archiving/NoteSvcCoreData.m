c021aabbaba4cc4a1d5fa67b8d95d0a748bc8e60
Robert Holzhauser <Robert@Wisdomstrategy.com>
2015-06-21 21:38:02 -0600

    create stubbed out methods

--- week5_cdilly_Archiving/NoteSvcCoreData.m
+++ week5_cdilly_Archiving/NoteSvcCoreData.m
@@ -26,14 +26,50 @@ NSMutableArray *notesArrayCoreData = nil;
 }
 
 - (Note *) deleteNote:(Note *)note{
+    NSFetchRequest *fetchRequest = [[NSFetchRequest alloc] init];
+    NSEntityDescription *entity = [NSEntityDescription entityForName:@"Note" inManagedObjectContext:moc];
+    NSPredicate *predicate = [NSPredicate predicateWithFormat:@"noteText = %@", note.noteText];
+    [fetchRequest setPredicate:predicate];
+    [fetchRequest setEntity:entity];
+    
+    NSError *error;
+    NSArray *fetchedObjects = [moc executeFetchRequest:fetchRequest error:&error];
+    
+    if ([fetchedObjects count] == 1)
+    {
+        NSManagedObject *managedObject = [fetchedObjects objectAtIndex:0];
+        [moc deleteObject:managedObject];
+    }
     return note;
 }
 
 - (Note *) addNote: (Note *) note {
+    NSManagedObject *managedObject =
+    [NSEntityDescription insertNewObjectForEntityForName:@"Note" inManagedObjectContext:moc];
+    [managedObject setValue:note.noteText forKey:@"noteText"];
+    NSError *error;
+    if (![moc save:&error]){
+        NSLog(@"addNote ERROR: %@", [error localizedDescription]);
+    }
     return note;
 }
 
 - (NSMutableArray *) retrieveAllNotes {
+    NSFetchRequest *fetchRequest = [[NSFetchRequest alloc] init];
+    NSEntityDescription *entity = [NSEntityDescription entityForName:@"Note" inManagedObjectContext:moc];
+    
+    [fetchRequest setEntity:entity];
+    NSError *error;
+    NSArray *fetchedObjects = [moc executeFetchRequest:fetchRequest error:&error];
+    NSMutableArray *notes = [NSMutableArray arrayWithCapacity:fetchedObjects.count];
+    
+    for (NSManagedObject *manageObject in fetchedObjects) {
+        Note *note = [[Note alloc] init];
+        note.noteText = [manageObject valueForKey:@"noteText"];
+        NSLog(@"note: %@ ", [note description]);
+        [notes addObject:note];
+    }
+    
     return notesArrayCoreData;
 }
 
